d2622c529d73e06fdac217b98d550d955da822f5
benjec83 <ben_carney@yahoo.com>
2022-12-29 17:35:18 -0800

    Initial Commit

---
+++ Comic Reader/Comic_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Comic_ReaderApp.swift
+//  Comic Reader
+//
+//  Created by Ben Carney on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Comic_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
